30b743e9247ec5b034225d27064075270bd522f0
Jacob Ransom <jacob@ransomdigital.com>
2026-02-18 23:20:19 +0000

    File changes

--- functions/getSetterPerformanceData.ts
+++ functions/getSetterPerformanceData.ts
@@ -12,6 +12,18 @@ async function fetchAll(entityRef, sort, pageSize = 5000) {
   return all;
 }
 
+async function fetchAllFiltered(entityRef, filter, sort, pageSize = 5000) {
+  let all = [];
+  let skip = 0;
+  while (true) {
+    const page = await entityRef.filter(filter, sort, pageSize, skip);
+    all = all.concat(page);
+    if (page.length < pageSize) break;
+    skip += pageSize;
+  }
+  return all;
+}
+
 Deno.serve(async (req) => {
   try {
     const base44 = createClientFromRequest(req);
@@ -23,11 +35,23 @@ Deno.serve(async (req) => {
       return Response.json({ error: 'Forbidden' }, { status: 403 });
     }
 
+    const body = await req.json().catch(() => ({}));
+    // Default to 90 days back if no dates provided
+    const now = new Date();
+    const defaultStart = new Date(now);
+    defaultStart.setDate(defaultStart.getDate() - 90);
+    const startDate = body.startDate || defaultStart.toISOString().split('T')[0];
+    const endDate = body.endDate || now.toISOString().split('T')[0];
+
     const sr = base44.asServiceRole.entities;
 
+    const leadFilter = {
+      created_date: { $gte: startDate + 'T00:00:00.000Z', $lte: endDate + 'T23:59:59.999Z' }
+    };
+
     const [clients, leads, users] = await Promise.all([
-      sr.Client.list(),
-      fetchAll(sr.Lead, '-created_date'),
+      fetchAll(sr.Client, '-created_date'),
+      fetchAllFiltered(sr.Lead, leadFilter, '-created_date'),
       sr.User.list(),
     ]);
 
